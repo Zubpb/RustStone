0ef343115cbde49fb736242e711aa7c951ab7615
Zubpb <xzub20@gmail.com>
2026-06-09 14:28:57 +0200

    remove target from tracking

--- target/debug/deps/ruststone.d
+++
@@ -1,5 +0,0 @@
-C:\Users\osha\Documents\VSC\ruststone\target\debug\deps\ruststone.d: src\main.rs
-
-C:\Users\osha\Documents\VSC\ruststone\target\debug\deps\ruststone.exe: src\main.rs
-
-src\main.rs:
--- target/debug/ruststone.d
+++
@@ -1,1 +0,0 @@
-C:\Users\osha\Documents\VSC\ruststone\target\debug\ruststone.exe: C:\Users\osha\Documents\VSC\ruststone\src\main.rs
